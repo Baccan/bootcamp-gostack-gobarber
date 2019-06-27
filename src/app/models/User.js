87e6d5618cbea29f8d3f9ca2e12d1db28d0498a0
Gustavo Baccan Gomes <gustavo.gomes@butantan.gov.br>
2019-06-27 17:01:51 -0300

    Model de usuario

---
+++ src/app/models/User.js
@@ -0,0 +1,22 @@
+import Sequelize, { Model } from 'sequelize';
+
+class User extends Model {
+  // Método chamado automaticamente pelo sequelize
+  static init(sequelize) {
+    // Chama o método init da classe Model e define as colunas que podem ser criadas por usuarios
+    super.init(
+      // Objeto que contem nas colunas
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password_hash: Sequelize.STRING,
+        provider: Sequelize.BOOLEAN,
+      },
+      {
+        sequelize,
+      }
+    );
+  }
+}
+
+export default User;
