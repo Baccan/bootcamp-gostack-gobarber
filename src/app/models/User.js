f654b904ab460f9c0c184996fdef7795db001dce
Gustavo Baccan Gomes <gustavo.gomes@butantan.gov.br>
2019-06-28 11:17:19 -0300

    Gerando hash de senha

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
   // Método chamado automaticamente pelo sequelize.
@@ -7,9 +8,11 @@ class User extends Model {
     // Chama o método init da classe Model e define as colunas que podem ser criadas por usuarios
     super.init(
       // Objeto que contem nas colunas
+      // VIRTUAL - Campo que existe apenas para verificação. Não armazena na base de dados
       {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
         provider: Sequelize.BOOLEAN,
       },
@@ -17,6 +20,21 @@ class User extends Model {
         sequelize,
       }
     );
+
+    // Hooks são techos de código que são executados automatcamente com ações realizadas no Model
+    // Apagando beforeSave e dando ctrl + space vc pode ver todoas as opções
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        console.log(user.password);
+        // 8 é a força da criptografia. Valores muito altos acabam pesando na aplicação
+        user.password_hash = await bcrypt.hash(user.password, 8);
+        console.log(user.password_hash);
+      }
+
+      console.log(user.password_hash);
+    });
+    // Retorna o model que acaba de ser inicializado
+    return this;
   }
 }
 
