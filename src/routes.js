de84b0ee9c283aa5e4c4a36046fb096019d87969
Baccan <gustavobaccang@gmail.com>
2019-07-09 01:48:48 +0000

    Listagem de agendamentos do usuário

--- src/routes.js
+++ src/routes.js
@@ -21,6 +21,7 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
+routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
 
 routes.get('/providers', ProviderController.index);
