60c5356df97fb1153035d767e01018a6457cf3d8
Baccan <gustavobaccang@gmail.com>
2019-07-09 21:27:08 +0000

    Listagem de horários disponíveis

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -23,6 +24,9 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
+routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
+
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
 routes.delete('/appointments/:id', AppointmentController.delete);
@@ -31,8 +35,6 @@ routes.get('/schedules', ScheduleController.index);
 
 routes.get('/notifications', NotificationController.index);
 routes.put('/notifications/:id', NotificationController.update);
-
-routes.get('/providers', ProviderController.index);
 
 // Rota para post de 1 unico arquivo (single) recepida pelo req.file
 routes.post('/files', upload.single('file'), FileController.store);
