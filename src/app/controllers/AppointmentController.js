cf8ec1f81ebf124c92d17f509bf2657b2c92c05b
Baccan <gustavobaccang@gmail.com>
2019-07-09 20:47:35 +0000

    Configuração de fila com Redis

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -6,7 +6,8 @@ import File from '../models/File';
 import Appointment from '../models/Appointment';
 import Notification from '../schemas/Notification';
 
-import Mail from '../../lib/Mail';
+import CancellationMail from '../jobs/CancellationMail';
+import Queue from '../../lib/Queue';
 
 class AppointmentController {
   async index(req, res) {
@@ -166,25 +167,8 @@ class AppointmentController {
     await appointment.save();
 
     // Enviar email
-    await Mail.sendMail({
-      to: `${appointment.provider.name} <${appointment.provider.email}>`,
-      subject: 'Agendamento cancelado',
-      // poderia ser html, text, template...
-      template: 'cancellation',
-      // variaveis a serem passadas para o template
-      context: {
-        provider: appointment.provider.name,
-        user: appointment.user.name,
-        date: format(
-          appointment.date,
-          // formato da data
-          // tudo oq está em aspas simples não será considerado para a formatação
-          // "dia - seria 22ia / d'dia' - seria 22dia"
-          "'dia' dd 'de' MMMM', às' H:mm'h'",
-          // Ex: dia 22 de Junho, às 8:40h
-          { locale: pt }
-        ),
-      },
+    await Queue.add(CancellationMail.key, {
+      appointment,
     });
 
     return res.json(appointment);
---
+++ src/queue.js
@@ -0,0 +1,4 @@
+import Queue from './lib/Queue';
+
+// arquivo separado para processar a fila em outra aplicação para não influenciar na performance
+Queue.processQueue();
